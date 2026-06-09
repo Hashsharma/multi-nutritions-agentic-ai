e6c44c841db1e31851654bad88b08b876773044b
anand <hashsharma0@gmail.com>
2026-06-09 23:04:55 +0530

    Nutrition router

--- nutrition_multiagents/api/v1/router.py
+++ nutrition_multiagents/api/v1/router.py
@@ -4,7 +4,7 @@ api_router = APIRouter()
 
 from nutrition_multiagents.api.v1.endpoints import profile, documents_router
 
-api_router.include_router(profile.router, prefix="/user", tags=["profile"])
+api_router.include_router(profile.router, prefix="/user", tags=["user"])
 api_router.include_router(documents_router.router, prefix="/documents", tags=["documents"])
 
 
